fe61ec57c514e80745f9cffa4bec1f095821db3b
Cheasy Jay <jamohgamer@gmail.com>
2024-07-15 11:16:19 +0300

    initial commit

--- server/models.py
+++ server/models.py
@@ -1,18 +1,24 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData, event
 from sqlalchemy_serializer import SerializerMixin
-
-db = SQLAlchemy()
-
 from datetime import datetime
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
 
 class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String)
-    username = db.Column(db.String)
-    created_at = db.Column(db.DateTime, default=datetime.now) 
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    content = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
 
-    def __repr__(self):
-        return f'<Message {self.body}>'
+# Automatically update 'updated_at' field to the current time whenever the record is updated
+@event.listens_for(Message, 'before_update')
+def receive_before_update(mapper, connection, target):
+    target.updated_at = datetime.utcnow()
